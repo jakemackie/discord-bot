5a12603add6a38c5448c3fe8bc78320bfa5e9276
jakemackie <jake.wmackie@gmail.com>
2024-10-26 22:51:51 +0100

    feat: Update ping command

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,12 +1,20 @@
 module.exports = {
   name: "ping",
-  description: "test command",
+  description: "test cmd",
   // deleted: Boolean,
-  // devOnly: Boolean,
+  devOnly: true,
   // testOnly: Boolean,
   // ? option: Object[] - see docs for more info
 
-  callback: (client, interaction) => {
-    interaction.reply("Pong!");
+  callback: async (client, interaction) => {
+    await interaction.deferReply();
+
+    const reply = await interaction.fetchReply();
+
+    const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+    interaction.editReply(
+      `Pong! Client ${ping}ms | Websocket: ${client.ws.ping}ms`
+    );
   },
 };
